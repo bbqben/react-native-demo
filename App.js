01de3e4dd716d43e85e8235092de91f757c161ec
Ben Tran <bbentran@gmail.com>
2018-06-29 02:36:29 -0400

    ✨ Initialized constructor and state

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
   render() {
     return (
       <View style={styles.container}>
